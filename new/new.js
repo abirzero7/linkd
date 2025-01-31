05b8d6ebd254186d07686afc657f6b0550237a40
abirzero7 <abir4605@gmail.com>
2025-02-01 01:15:59 +0600

    new.js

--- new/new.js
+++ new/new.js
@@ -1,5 +1,27 @@
-let x= "u"
-let y= "ah";
+//let x= "hey "
+//let y= " so much ";
 
-z = x+y.padStart(10000,("m"));
+//z = x+y.padStart(100,("i love you "));
+//console.log(z)
+
+const text=document.getElementById("input");
+
+const submit=document.getElementById("submit");
+const label=document.getElementById("label");
+
+submit.onclick=function(){
+    
+let pad1=(text.value);
+
+let x= "U";
+let y= "mah";
+
+let z=x+y.padStart(pad1,"m");
+label.textContent=z;
+
 console.log(z)
+
+
+}
+
+
